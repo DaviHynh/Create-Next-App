92abb28d5d54a78a318c8a4af1d50284b627355d
David Huynh <133894052+DaviHynh@users.noreply.github.com>
2023-08-30 10:17:55 -0400

    Added a link example for future reference. (#1)
    * Create page.js
    
    * Update page.js
    
    * Update page.js

---
+++ src/app/(users)/calculator/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function calculator () {
+    return (
+        <div>
+            <title>DS Season Calculator</title>
+            <h1>Hello World!</h1>
+            <Link href="/">Home</Link>
+        </div>
+    )
+}
